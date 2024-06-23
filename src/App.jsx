0eb9b817a85d09f91e8966fe94b29afd0c47d6ee
Vinay Pratap <89694344+thevinaypratap@users.noreply.github.com>
2024-06-23 15:08:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,7 @@ import "./index.css";
 function App() {
   return (
     <>
-      <ul>
-        <li>Sum of two number: {Add(40, 4)}</li>
-        <li>Sub of two number: {Sub(30, 3)}</li>
-        <li>Mul of two number: {Mul(40, 4)}</li>
-        <li>Div of two number: {Div(40, 3)}</li>
-      </ul>
-
-      <Heading />
-      {/* <Para />
-      <List /> */}
+     
     </>
   );
 }
